2e7bae70b4483001997e59b094c9099e560f1851
jhemi-cristina <jhemilycristina@gmail.com>
2022-05-25 13:12:13 -0300

    feat: Clean Code Principles and Declarative Programming

--- script.js
+++ script.js
@@ -8,15 +8,36 @@ let minutes;
 const minutesDisplay = document.querySelector(".minutes");
 const secondsDisplay = document.querySelector(".seconds");
 
+function resetControls() {
+  buttonPlay.classList.remove("hide");
+  buttonPause.classList.add("hide");
+  buttonSet.classList.remove("hide");
+  buttonStop.classList.add("hide");
+}
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  secondsDisplay.textContent = String(seconds).padStart(2, "0");
+}
+
 function countdown() {
   setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent);
+    let minutes = Number(minutesDisplay.textContent);
 
-    if (seconds <= 0) {
-      seconds = 60;
+    updateTimerDisplay(minutes, 0);
+
+    if (minutes <= 0) {
+      resetControls();
+      return;
     }
 
-    secondsDisplay.textContent = seconds - 1;
+    if (seconds <= 0) {
+      seconds = 2;
+      --minutes;
+    }
+
+    updateTimerDisplay(minutes, String(seconds - 1));
 
     countdown();
   }, 1000);
@@ -37,10 +58,7 @@ buttonPause.addEventListener("click", function () {
 });
 
 buttonStop.addEventListener("click", function () {
-  buttonPlay.classList.remove("hide");
-  buttonPause.classList.add("hide");
-  buttonSet.classList.remove("hide");
-  buttonStop.classList.add("hide");
+  resetControls();
 });
 
 buttonSoundOff.addEventListener("click", function () {
@@ -55,5 +73,5 @@ buttonSoundOn.addEventListener("click", function () {
 
 buttonSet.addEventListener("click", function () {
   minutes = prompt("Quantos minutos?");
-  minutesDisplay.textContent = minutes;
+  updateTimerDisplay(minutes, 0);
 });
