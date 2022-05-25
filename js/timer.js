e48b6f4bc47a242d9422eaedc995ccedee81328c
jhemi-cristina <jhemilycristina@gmail.com>
2022-05-25 15:51:24 -0300

    finalization

--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,23 @@
-export function Timer({
+import Sounds from "./sounds.js";
+
+export default function Timer({
   minutesDisplay,
   secondsDisplay,
-  timerTimeOut,
   resetControls,
+  sound,
 }) {
-  function updateTimerDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  let timerTimeOut;
+  let minutes = Number(minutesDisplay.textContent);
+
+  function updateDisplay(newMinutes, seconds) {
+    newMinutes = newMinutes === undefined ? minutes : newMinutes;
+    seconds = seconds === undefined ? 0 : seconds;
+    minutesDisplay.textContent = String(newMinutes).padStart(2, "0");
     secondsDisplay.textContent = String(seconds).padStart(2, "0");
   }
 
-  function resetTimer() {
-    updateTimerDisplay(minutes, 0);
+  function reset() {
+    updateDisplay(minutes, 0);
     clearTimeout(timerTimeOut);
   }
 
@@ -18,27 +25,41 @@ export function Timer({
     timerTimeOut = setTimeout(function () {
       let seconds = Number(secondsDisplay.textContent);
       let minutes = Number(minutesDisplay.textContent);
+      let isFinished = minutes <= 0 && seconds <= 0;
 
-      updateTimerDisplay(minutes, 0);
+      updateDisplay(minutes, 0);
 
-      if (minutes <= 0) {
+      if (isFinished) {
         resetControls();
+        updateDisplay();
+        Sounds().timeEnd();
         return;
       }
 
       if (seconds <= 0) {
-        seconds = 3;
+        seconds = 60;
         --minutes;
       }
 
-      updateTimerDisplay(minutes, String(seconds - 1));
+      updateDisplay(minutes, String(seconds - 1));
 
       countdown();
     }, 1000);
   }
 
+  function updateMinutes(newMinutes) {
+    minutes = newMinutes;
+  }
+
+  function hold() {
+    clearTimeout(timerTimeOut);
+  }
+
   return {
     countdown,
-    resetTimer,
+    reset,
+    updateDisplay,
+    updateMinutes,
+    hold,
   };
 }
