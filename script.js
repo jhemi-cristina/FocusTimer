35b7ce71cc454b1d2aa18abf7476e42402d2615b
jhemi-cristina <jhemilycristina@gmail.com>
2022-05-25 11:05:31 -0300

    feat: Event-driven, imperative programming and callbacks

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 // Document Object Model
 
 let play = document.querySelector(".play");
+let pause = document.querySelector(".pause");
+// Event-driven
+// programação imperativa
+// callback
+play.addEventListener("click", function () {
+  play.classList.add("hide");
+  pause.classList.remove("hide");
+});
 
-play.classList.add("hide");
-document.querySelector(".pause").classList.remove("hide");
+pause.addEventListener("click", function () {
+  pause.classList.add("hide");
+  play.classList.remove("hide");
+});
