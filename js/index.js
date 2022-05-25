b181d8dab5ea1896270b50df535aa4e99222fc89
jhemi-cristina <jhemilycristina@gmail.com>
2022-05-25 14:13:22 -0300

    feat: Creating objects with the Factory pattern and dependency injection

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+// default import
+import resetControls from "./controls.js";
+import { Timer } from "./timer.js";
+
 const buttonPlay = document.querySelector(".play");
 const buttonPause = document.querySelector(".pause");
 const buttonStop = document.querySelector(".stop");
@@ -9,45 +13,12 @@ const secondsDisplay = document.querySelector(".seconds");
 let minutes = Number(minutesDisplay.textContent);
 let timerTimeOut;
 
-function resetControls() {
-  buttonPlay.classList.remove("hide");
-  buttonPause.classList.add("hide");
-  buttonSet.classList.remove("hide");
-  buttonStop.classList.add("hide");
-}
-
-function updateTimerDisplay(minutes, seconds) {
-  minutesDisplay.textContent = String(minutes).padStart(2, "0");
-  secondsDisplay.textContent = String(seconds).padStart(2, "0");
-}
-
-function resetTimer() {
-  updateTimerDisplay(minutes, 0);
-  clearTimeout(timerTimeOut);
-}
-
-function countdown() {
-  timerTimeOut = setTimeout(function () {
-    let seconds = Number(secondsDisplay.textContent);
-    let minutes = Number(minutesDisplay.textContent);
-
-    updateTimerDisplay(minutes, 0);
-
-    if (minutes <= 0) {
-      resetControls();
-      return;
-    }
-
-    if (seconds <= 0) {
-      seconds = 3;
-      --minutes;
-    }
-
-    updateTimerDisplay(minutes, String(seconds - 1));
-
-    countdown();
-  }, 1000);
-}
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  timerTimeOut,
+  resetControls,
+});
 
 buttonPlay.addEventListener("click", function () {
   buttonPlay.classList.add("hide");
@@ -55,7 +26,7 @@ buttonPlay.addEventListener("click", function () {
   buttonSet.classList.add("hide");
   buttonStop.classList.remove("hide");
 
-  countdown();
+  timer.countdown();
 });
 
 buttonPause.addEventListener("click", function () {
@@ -66,7 +37,7 @@ buttonPause.addEventListener("click", function () {
 
 buttonStop.addEventListener("click", function () {
   resetControls();
-  resetTimer();
+  timer.resetTimer();
 });
 
 buttonSoundOff.addEventListener("click", function () {
@@ -82,7 +53,7 @@ buttonSoundOn.addEventListener("click", function () {
 buttonSet.addEventListener("click", function () {
   let newMinutes = prompt("Quantos minutos?");
   if (!newMinutes) {
-    resetTimer();
+    timer.resetTimer();
     return;
   }
 
