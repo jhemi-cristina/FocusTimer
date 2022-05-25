228af8c6ff31aa38c83d5c8d459ff05275b4e5f7
jhemi-cristina <jhemilycristina@gmail.com>
2022-05-25 11:33:07 -0300

    feat: Starting the logic for the counter

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-// DOM
-// Document Object Model
-
-// refatoração: mudar um código para deixá-lo mais entendível
-// deixar o código mais performático
-// SEM ALTERAR suas funcionalidades
 const buttonPlay = document.querySelector(".play");
 const buttonPause = document.querySelector(".pause");
 const buttonStop = document.querySelector(".stop");
 const buttonSet = document.querySelector(".set");
 const buttonSoundOn = document.querySelector(".sound-on");
 const buttonSoundOff = document.querySelector(".sound-off");
+let minutes;
+const minutesDisplay = document.querySelector(".minutes");
+const secondsDisplay = document.querySelector(".seconds");
 
-// Event-driven
-// programação imperativa
-// callback
+function countdown() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent);
+
+    if (seconds <= 0) {
+      seconds = 60;
+    }
+
+    secondsDisplay.textContent = seconds - 1;
+
+    countdown();
+  }, 1000);
+}
+
 buttonPlay.addEventListener("click", function () {
   buttonPlay.classList.add("hide");
   buttonPause.classList.remove("hide");
   buttonSet.classList.add("hide");
   buttonStop.classList.remove("hide");
+
+  countdown();
 });
 
 buttonPause.addEventListener("click", function () {
@@ -41,4 +51,9 @@ buttonSoundOff.addEventListener("click", function () {
 buttonSoundOn.addEventListener("click", function () {
   buttonSoundOn.classList.add("hide");
   buttonSoundOff.classList.remove("hide");
+});
+
+buttonSet.addEventListener("click", function () {
+  minutes = prompt("Quantos minutos?");
+  minutesDisplay.textContent = minutes;
 });
